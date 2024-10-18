edf31dedd6a322e7ec9cd8cafcc6be8ade688d96
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-18 14:53:25 +0200

    added random number generator and summed with input number only if conditions are true

--- js-Pari e Dispari/js/main.js
+++ js-Pari e Dispari/js/main.js
@@ -1,9 +1,20 @@
+// inserimento dei valori da parte dell'utente
 const inputEvenOdd = prompt('Scrivi pari o dispari');
-const inputNumber = prompt('Scegli un numero compreso tra 1 e 5');
+const inputNumber = parseInt(prompt('Scegli un numero compreso tra 1 e 5'));
 
+//  verifica che i valori inseriti dall'utente rispettino i criteri richiesti
 if ((inputEvenOdd === 'pari' || inputEvenOdd === 'dispari') && 
     (!isNaN(inputNumber)) && (inputNumber >= 1 && inputNumber <= 5)){
-    console.log(`hai scelto ${inputEvenOdd}`);
+    const sum = getRndInteger(1, 5) + inputNumber;
+    console.log(sum);
 } else {
     alert('Per favore scrivi la tua scelta tra dispari e pari ed inserisci un numero compreso tra 1 e 5')
+};
+
+
+/********
+FUNZIONI
+********/
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
