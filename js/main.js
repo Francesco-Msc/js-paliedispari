6a7e27a88c2240db53e6715b567d271de14b584c
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-18 14:11:25 +0200

    added conditions and comments to program and reorganized

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
+/********
+PROGRAMMA
+********/
+
 // input parola da analizzare
 const wordInput = prompt('Inserisci una parola');
+
+// inverto l'ordine della parola e la salvo cosi da poterla usare nella risposta
+const wordReversed = wordInput.split('') .reverse() .join('');
+
+// risultato analizzato dalla funzione
+const result = isPalindrome(wordInput)
+
+// condizine per cui se la parola è palindroma esegue un'azione altrimenti ne esegue un'altra
+if (result){
+    console.log(`la parola "${wordInput}" è palindroma`);
+} else {
+    console.log(`la parola "${wordInput}" non è palindroma, eccola al contrario "${wordReversed}"`);
+}
+
+/********
+FUNZIONE
+********/
 
 /**
  * verifica se la parola inserita dall'utente letta al contrario è uguale alla parola di partenza
  * @param {string} wordInput
  * @returns {boolean}
- */
+*/
 function isPalindrome(wordInput) {
     const wordReversed = wordInput.split('') .reverse() .join('');
     return wordReversed === wordInput;
 }
-
-const result = isPalindrome(wordInput)
-console.log(result);
