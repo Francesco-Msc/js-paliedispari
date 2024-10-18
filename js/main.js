453eee475e61ba8ea9f7874bfda3d02bec2ab1a4
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-18 13:35:50 +0200

    simplified code

--- js/main.js
+++ js/main.js
@@ -2,12 +2,5 @@ const wordInput = prompt('Inserisci una parola');
 console.log(wordInput);
 
 
-const word = wordInput.split('');
-console.log(word);
-
-const reverse = word.reverse();
-console.log(reverse);
-
-const wordReversed = reverse.join('');
+const wordReversed = wordInput.split('') .reverse() .join('');
 console.log(wordReversed);
-
