1b6bf993dc80553e2e6ee26b5638c07c152900af
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-18 13:33:10 +0200

    reversed the array and transformed to string

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const wordInput = prompt('Inserisci una parola');
-console.log(word);
+console.log(wordInput);
 
 
 const word = wordInput.split('');
 console.log(word);
- 
+
+const reverse = word.reverse();
+console.log(reverse);
+
+const wordReversed = reverse.join('');
+console.log(wordReversed);
+
