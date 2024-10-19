0569211bcd124adf29216306e1b27ec267cce5a7
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-19 11:24:29 +0200

    added condition of empty input or single letter

--- js-palindroma/js/main.js
+++ js-palindroma/js/main.js
@@ -3,21 +3,31 @@ PROGRAMMA
 ********/
 
 // input parola da analizzare
-const wordInput = prompt('Inserisci una parola');
+const wordInput = (prompt('Inserisci una parola')).toLowerCase;
 const displayResult = document.getElementById('result')
+let text;
 
-// inverto l'ordine della parola e la salvo cosi da poterla usare nella risposta
-const wordReversed = wordInput.split('') .reverse() .join('');
-
-// risultato analizzato dalla funzione
-const result = isPalindrome(wordInput)
-
-// condizine per cui se la parola è palindroma esegue un'azione altrimenti ne esegue un'altra
-if (result){
-    text = `la parola "${wordInput}" è palindroma`;
+// condizione per cui se il campo è vuoto non puo essere considerato, invece se è composto da un solo carattere è palindroma per forza
+if (wordInput.length === 0){
+    text = 'Un campo vuoto non è considerato come parola palindroma'
+} else if (wordInput.length === 1){
+    text = `la lettera "${wordInput}" è palindroma`;
 } else {
-    text = `la parola "${wordInput}" non è palindroma, eccola al contrario "${wordReversed}"`;
+    
+    // inverto l'ordine della parola e la salvo cosi da poterla usare nella risposta
+    const wordReversed = wordInput.split('') .reverse() .join('');
+    
+    // risultato analizzato dalla funzione
+    const result = isPalindrome(wordInput)
+    
+    // condizine per cui se la parola è palindroma esegue un'azione altrimenti ne esegue un'altra
+    if (result){
+        text = `la parola "${wordInput}" è palindroma`;
+    } else {
+        text = `la parola "${wordInput}" non è palindroma, eccola al contrario "${wordReversed}"`;
+    }
 }
+
 
 displayResult.innerHTML = text;
 
